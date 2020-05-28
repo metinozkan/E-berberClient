8d6932187da3b46a6d09703c7f5d4ecca4d63502
metinozkn <metin.ozkn@hotmail.com>
2020-05-28 13:05:34 +0300

    hourCalender re edit, added new date month, day in hourCalender

--- src/Components/HourCalender.js
+++ src/Components/HourCalender.js
@@ -33,6 +33,7 @@ class HourCalender extends Component {
       selectedDate: null,
       staffFreeHoursWeekly: this.props.staffFreeHoursWeekly,
       daysWithFreeHours: [],
+      getMonth: "",
     };
   }
   //state,updateState
@@ -48,10 +49,73 @@ class HourCalender extends Component {
     );
   };
 
+  hoursColumnStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: "100%",
+    overflow: "auto",
+  };
+
+  EmptyHoursComp = () => {
+    return (
+      <div style={{ padding: ".5em", cursor: "pointer" }} onClick={() => {}}>
+        Randevu yok
+      </div>
+    );
+  };
+
   componentDidMount = () => {
     // this.createDays();
+    var date = new Date();
+    var diffMonday =
+      date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+    var diffTuesday =
+      date.getDate() - date.getDay() + (date.getDay() === 0 ? -5 : 2);
+    var diffWednesday =
+      date.getDate() - date.getDay() + (date.getDay() === 0 ? -4 : 3);
+    var diffThursday =
+      date.getDate() - date.getDay() + (date.getDay() === 0 ? -3 : 4);
+
+    var diffFriday =
+      date.getDate() - date.getDay() + (date.getDay() === 0 ? -2 : 5);
+    var diffSaturday =
+      date.getDate() - date.getDay() + (date.getDay() === 0 ? -2 : 6);
+    var diffSunday =
+      date.getDate() - date.getDay() + (date.getDay() === 0 ? 0 : 7);
+
+    this.setState({
+      getMonth: date.getMonth(),
+      getDaysArray: [
+        diffMonday,
+        diffTuesday,
+        diffWednesday,
+        diffThursday,
+        diffFriday,
+        diffSaturday,
+        diffSunday,
+      ],
+    });
+    // console.log(
+    //   "diff",
+    //   date.getDate(),
+    //   "-----",
+    //   date.getDay(),
+    //   "---",
+    //   date.getDay(),
+    //   "----",
+    //   diffMonday,
+    //   "---",
+    //   diffTuesday,
+    //   "----",
+    //   date.getMonth()
+    // );
+    console.log(new Date(date.setDate(diffTuesday)));
   };
   render() {
+    var date = new Date();
+
     return (
       <Grid
         container
@@ -64,7 +128,7 @@ class HourCalender extends Component {
       >
         <div
           style={{
-            height: "580px",
+            height: "auto",
             width: "100%",
             display: "flex",
             flexDirection: "row",
@@ -92,7 +156,7 @@ class HourCalender extends Component {
               }}
             />
           </Grid> */}
-          {dAys
+          {/* {dAys
             // .slice(this.state.countShowDays, this.state.countShowDays + 6)
             .map((x) => (
               <Grid
@@ -134,7 +198,308 @@ class HourCalender extends Component {
                   ))}
                 </Grid>
               </Grid>
-            ))}
+            ))} */}
+
+          <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>Monday</div>
+            <Grid item xs={12} style={this.hoursColumnStyle}>
+              {date.getDay() <= 1 && this.state.staffFreeHoursWeekly.Monday ? (
+                this.state.staffFreeHoursWeekly.Monday.map((h) => (
+                  <div
+                    style={{
+                      padding: ".5em",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      console.log(
+                        "yıl:",
+                        2020,
+                        "ay:",
+                        this.state.getMonth,
+                        "gün:",
+                        this.state.getDaysArray[0],
+                        "saat:",
+                        h
+                      );
+                    }}
+                  >
+                    {h}
+                  </div>
+                ))
+              ) : (
+                <this.EmptyHoursComp />
+              )}
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>Tuesday</div>
+            <Grid item xs={12} style={this.hoursColumnStyle}>
+              {date.getDay() <= 2 && this.state.staffFreeHoursWeekly.Tuesday ? (
+                this.state.staffFreeHoursWeekly.Tuesday.map((h) => (
+                  <div
+                    style={{ padding: ".5em", cursor: "pointer" }}
+                    onClick={() => {
+                      console.log(
+                        "yıl:",
+                        2020,
+                        "ay:",
+                        this.state.getMonth,
+                        "gün:",
+                        this.state.getDaysArray[1],
+                        "saat:",
+                        h
+                      );
+                    }}
+                  >
+                    {h}
+                  </div>
+                ))
+              ) : (
+                <this.EmptyHoursComp />
+              )}
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>
+              Wednesday
+            </div>
+            <Grid item xs={12} style={this.hoursColumnStyle}>
+              {date.getDay() <= 3 &&
+              this.state.staffFreeHoursWeekly.Wednesday ? (
+                this.state.staffFreeHoursWeekly.Wednesday.map((h) => (
+                  <div
+                    style={{ padding: ".5em", cursor: "pointer" }}
+                    onClick={() => {
+                      console.log(
+                        "yıl:",
+                        2020,
+                        "ay:",
+                        this.state.getMonth,
+                        "gün:",
+                        this.state.getDaysArray[2],
+                        "saat:",
+                        h
+                      );
+                    }}
+                  >
+                    {h}
+                  </div>
+                ))
+              ) : (
+                <this.EmptyHoursComp />
+              )}
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>Thursday</div>
+            <Grid item xs={12} style={this.hoursColumnStyle}>
+              {date.getDay() <= 4 &&
+              this.state.staffFreeHoursWeekly.Thursday ? (
+                this.state.staffFreeHoursWeekly.Thursday.map((h) => (
+                  <div
+                    style={{ padding: ".5em", cursor: "pointer" }}
+                    onClick={() => {
+                      console.log(
+                        "yıl:",
+                        2020,
+                        "ay:",
+                        this.state.getMonth,
+                        "gün:",
+                        this.state.getDaysArray[3],
+                        "saat:",
+                        h
+                      );
+                    }}
+                  >
+                    {h}
+                  </div>
+                ))
+              ) : (
+                <this.EmptyHoursComp />
+              )}
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>Friday</div>
+            <Grid item xs={12} style={this.hoursColumnStyle}>
+              {date.getDay() <= 5 && this.state.staffFreeHoursWeekly.Friday ? (
+                this.state.staffFreeHoursWeekly.Friday.map((h) => (
+                  <div
+                    style={{ padding: ".5em", cursor: "pointer" }}
+                    onClick={() => {
+                      console.log(
+                        "yıl:",
+                        2020,
+                        "ay:",
+                        this.state.getMonth,
+                        "gün:",
+                        this.state.getDaysArray[4],
+                        "saat:",
+                        h
+                      );
+                    }}
+                  >
+                    {h}
+                  </div>
+                ))
+              ) : (
+                <this.EmptyHoursComp />
+              )}
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>Saturday</div>
+            <Grid
+              item
+              xs={12}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {date.getDay() <= 6 &&
+              this.state.staffFreeHoursWeekly.Saturday ? (
+                this.state.staffFreeHoursWeekly.Saturday.map((h) => (
+                  <div
+                    style={{ padding: ".5em", cursor: "pointer" }}
+                    onClick={() => {
+                      console.log(
+                        "yıl:",
+                        2020,
+                        "ay:",
+                        this.state.getMonth,
+                        "gün:",
+                        this.state.getDaysArray[5],
+                        "saat:",
+                        h
+                      );
+                    }}
+                  >
+                    {h}
+                  </div>
+                ))
+              ) : (
+                <this.EmptyHoursComp />
+              )}
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>Sunday</div>
+            <Grid item xs={12} style={this.hoursColumnStyle}>
+              {date.getDay() <= 7 && this.state.staffFreeHoursWeekly.Sunday ? (
+                this.state.staffFreeHoursWeekly.Sunday.map((h) => (
+                  <div
+                    style={{ padding: ".5em", cursor: "pointer" }}
+                    onClick={() => {
+                      console.log(
+                        "yıl:",
+                        2020,
+                        "ay:",
+                        this.state.getMonth,
+                        "gün:",
+                        this.state.getDaysArray[6],
+                        "saat:",
+                        h
+                      );
+                    }}
+                  >
+                    {h}
+                  </div>
+                ))
+              ) : (
+                <this.EmptyHoursComp />
+              )}
+            </Grid>
+          </Grid>
+          {/* <Grid
+            item
+            xs={2}
+            style={{
+              textAlign: "center",
+              margin: ".5em",
+            }}
+          >
+            <div style={{ fontSize: "20px", fontWeight: "bold" }}>{x.day}</div>
+            <Grid
+              item
+              xs={12}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {x.emptyHour.map((h) => (
+                <div
+                  style={{ padding: ".5em", cursor: "pointer" }}
+                  onClick={() => {
+                    this.setState({
+                      selectedDate: { day: x.day, hour: h },
+                    });
+                    this.props.updateState({
+                      selectedDate: { day: x.day, hour: h },
+                      stepThreeActive: true,
+                    });
+                  }}
+                >
+                  {h}
+                </div>
+              ))}
+            </Grid>
+          </Grid> */}
+
           {/* <Grid
             item
             xs={1}
